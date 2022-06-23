1e12ae621e80f4086182f2bb741e3fc8be306731
KauanR <kau.matosr@gmail.com>
2022-06-22 23:17:24 -0300

    feat: enabling dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,25 @@
+import { createTheme, ThemeProvider } from '@mui/material'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import '../styles/main.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
+
+    const theme = createTheme({
+        palette: {
+            mode: 'dark'
+        }
+    })
+
     return (
         <>
             <Head>
                 <title>OCR Demo</title>
             </Head>
             <main>
-                <Component {...pageProps} />
+                <ThemeProvider theme={theme}>
+                    <Component {...pageProps} />
+                </ThemeProvider>
             </main>
         </>
     )
