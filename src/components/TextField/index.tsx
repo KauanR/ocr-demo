5f541591912753149a123ffe5e82af11f4bb0d82
KauanR <kau.matosr@gmail.com>
2022-06-22 23:16:33 -0300

    feat: text field wrapper implementation

---
+++ src/components/TextField/index.tsx
@@ -0,0 +1,22 @@
+import TextField from '@mui/material/TextField'
+import { useField } from 'formik'
+
+const TextFieldWrapper = ({name, showErrorMsg = true, ...otherProps}) => {
+    const [field, control] = useField(name)
+
+    const fieldConfig: any = {
+        ...field,
+        ...otherProps
+    }
+
+    if(control && control.touched && control.error) {
+        fieldConfig.error = true
+        fieldConfig.helperText = showErrorMsg && control.error
+    }
+
+    return (
+        <TextField {...fieldConfig} />
+    )
+}
+
+export default TextFieldWrapper
